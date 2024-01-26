bf65274005a02de3be9ae0ccad49aa0a00900c80
Anushka <banerjeeanushka154@gmail.com>
2024-01-26 09:09:37 +0530

    added comments in productController

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -35,6 +35,8 @@ const getAllProducts = async(req, res)=>{
         apiData = apiData.select(selectFix);
     }
 
+    //adding pagination
+
     let page = Number(req.query.page) || 1;
     let limit = Number(req.query.limit) || 8;
 
