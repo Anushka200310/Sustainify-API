f2d32fcda28fef56ca93c8367e0356f6f377067d
Anushka <banerjeeanushka154@gmail.com>
2024-03-03 14:14:24 +0530

    Enable CORS for all requests

--- server.js
+++ server.js
@@ -3,13 +3,24 @@ import connectDb from "./db/connectDb.js";
 import router from "./Routes/productRoutes.js";
 import dotenv from "dotenv";
 import middlewares from "./Middleware/middleware.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+    origin: "*", 
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "x-api-key"],
+  };
+  
+
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 app.use(middlewares);
 
